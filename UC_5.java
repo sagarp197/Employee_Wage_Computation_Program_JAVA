57bd180530e2ad14393efa2b7ddb9ffb59a02e00
sagarp197 <sagarp197@gmail.com>
2022-04-29 03:03:26 +0530

    Added UC_5

--- UC_5.java
+++ UC_5.java
@@ -1,16 +1,22 @@
 package com.bridgelabz;
 
-public class UC_4 {
+public class UC_5 {
+
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOUR = 8;
     private static final int HALF_DAY_HOUR = 4;
-
+    private static final int TOTAL_WORKING_DAYS = 20;
+    private static final int TOTAL_WORKING_HOURS = 100;
     private static final int IS_FULL_TIME = 1;
-
     private static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
-        int dailyWage = 0;
+        int dayCount = 1;
+        int workingHours = 0;
+        int totalWage = 0;
+
+        while (dayCount <= TOTAL_WORKING_DAYS && workingHours <= TOTAL_WORKING_HOURS) {
+            int dailyWage = 0;
         int empStatus = (int) (Math.random() * 10) % 3;
         System.out.println(empStatus);
 
@@ -18,14 +24,14 @@ public class UC_4 {
 
             case IS_FULL_TIME:
                 System.out.println("Employee worked FULL TIME");
-                //workingHours = workingHours + FULL_DAY_HOUR;
+                workingHours = workingHours + FULL_DAY_HOUR;
                 dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
                 System.out.println("Employees Wage for Day is: " + dailyWage);
                 break;
 
             case IS_PART_TIME:
                 System.out.println("Employee worked PART_TIME");
-                //workingHours = workingHours + HALF_DAY_HOUR;
+                workingHours = workingHours + HALF_DAY_HOUR;
                 dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
                 System.out.println("Employees Wage for Day is: " + dailyWage);
                 break;
@@ -33,5 +39,14 @@ public class UC_4 {
             default:
                 System.out.println("Employee is Absent");
         }
+            totalWage = totalWage + dailyWage;
+            System.out.println("Daily Wage for day : " + dayCount + " is " + dailyWage);
+            dayCount++;
+        }
+        System.out.println("Total Days : "+ (dayCount-1));
+        System.out.println("Total Working Hours : " + workingHours);
+        System.out.println("Total wage for a month :" +totalWage);
+
     }
 }
+
