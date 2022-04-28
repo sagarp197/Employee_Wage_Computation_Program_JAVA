f350085cdac1b958fa08739e9b788089ec9dc449
sagarp197 <sagarp197@gmail.com>
2022-04-29 02:43:01 +0530

    Added changes UC_4 to master

--- UC_4.java
+++ UC_4.java
@@ -3,7 +3,35 @@ package com.bridgelabz;
 public class UC_4 {
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOUR = 8;
+    private static final int HALF_DAY_HOUR = 4;
+
+    private static final int IS_FULL_TIME = 1;
+
+    private static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
+        int dailyWage = 0;
+        int empStatus = (int) (Math.random() * 10) % 3;
+        System.out.println(empStatus);
+
+        switch (empStatus) {
+
+            case IS_FULL_TIME:
+                System.out.println("Employee worked FULL TIME");
+                //workingHours = workingHours + FULL_DAY_HOUR;
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employees Wage for Day is: " + dailyWage);
+                break;
+
+            case IS_PART_TIME:
+                System.out.println("Employee worked PART_TIME");
+                //workingHours = workingHours + HALF_DAY_HOUR;
+                dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                System.out.println("Employees Wage for Day is: " + dailyWage);
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+        }
     }
 }
